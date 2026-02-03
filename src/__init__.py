f700edaf0b8776bdb65b404a82846efed3e73279
Lokesh Maheshwari <l.maheshwari2023@gamil.com>
2026-02-03 23:46:37 +0530

    made src folder & added _init_.py file

---
+++ src/__init__.py
@@ -0,0 +1,10 @@
+# src/__init__.py
+# package initializer for vehicle-detection src
+__all__ = [
+    "config",
+    "download_assets",
+    "yolo_loader",
+    "detector",
+    "video_processor",
+    "cli",
+]
