aba2b8badbd2804725586db8602d04a001ddcfb3
Lokesh Maheshwari <l.maheshwari2023@gamil.com>
2026-02-06 18:06:01 +0530

    Add missing import statements in package initializer

--- src/__init__.py
+++ src/__init__.py
@@ -1,5 +1,8 @@
 # src/__init__.py
 # package initializer for vehicle-detection src
+
+from . import config, download_assets, yolo_loader, detector, video_processor, cli
+
 __all__ = [
     "config",
     "download_assets",
